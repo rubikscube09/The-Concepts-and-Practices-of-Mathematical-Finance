d51c92d2c1bcd55ce64b949edc8a6eee626470dd
Abhimanyu <abhimanyuc25@gmail.com>
2020-11-06 15:29:52 -0500

    path generation done, need to work on vectorized hedging

--- pathgen.py
+++ pathgen.py
@@ -7,12 +7,27 @@ class SamplePath():
     Class that generates sample paths for monte-carlo simulation of options prices
     as well as for use in hedging simulations.
     '''
-    def __init__(self):
-        pass
-    def generate_path():
+    def __init__(self,dt,T,S0):
+        self.dt = dt
+        self.T = T
+        self.S0 = S0
+
+    def generate_paths():
         pass
 
 class GeometricBrownianMotion(SamplePath):
 
-    def __init__(self,mu,sigma,dt,T,method = 'Euler'):
-        mu = 
+    def __init__(self,mu,sigma,dt,T,S0,method = 'Euler'):
+        super().__init__(dt,T,S0)
+        self.mu = mu
+        self.sigma = sigma
+        self.method = method
+
+    def generate_paths(self,n_paths = 1):
+        num_samps = int(self.T/self.dt)
+        sample_paths = np.ndarray(shape = [n_paths,num_samps])
+        sample_paths[:,0] = np.array([self.S0]*n_paths)
+        sample_paths[:,1:] = np.exp(self.mu*self.dt + np.sqrt(self.dt)*self.sigma*np.random.randn(n_paths,num_samps - 1))
+        sample_paths = sample_paths.cumprod(axis = 1)
+        return sample_paths
+        
