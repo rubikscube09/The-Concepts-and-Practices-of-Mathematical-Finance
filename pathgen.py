a1328f5e6dacf128ed765f625d27c6ccf1d452a0
Abhimanyu <abhimanyuc25@gmail.com>
2020-11-06 13:27:16 -0500

    path generation module for generating sample paths for simulations

---
+++ pathgen.py
@@ -0,0 +1,18 @@
+import numpy as np 
+import scipy.stats
+import pandas as pd
+
+class SamplePath():
+    '''
+    Class that generates sample paths for monte-carlo simulation of options prices
+    as well as for use in hedging simulations.
+    '''
+    def __init__(self):
+        pass
+    def generate_path():
+        pass
+
+class GeometricBrownianMotion(SamplePath):
+
+    def __init__(self,mu,sigma,dt,T,method = 'Euler'):
+        mu = 
